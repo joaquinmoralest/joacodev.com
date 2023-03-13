5bebd056e9e3f4f672a39e707115fd5e6aed4f6e
Joaquin Morales <joaquin.moralestr@gmail.com>
2023-03-13 10:03:48 -0300

    feat: Add projects data and fix finish layout for cards

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <nav className='w-full p-5 flex justify-between bg-slate-900 border-b-zinc-100 text-slate-400 fixed z-10'>
       <div>
-        <p>Joaquin Morales</p>
+        <p>JoacoDev</p>
       </div>
       <div className='w-96 flex justify-between'>
         <Link className='hover:text-slate-200 transition ease-in-out duration-300' href='/'>Inicio</Link>
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,7 @@ import { db } from './_app'
 import Card from '../components/Card'
 import Stack from '../components/Stack'
 import SocialNetworks from '../components/SocialNetworks'
+import { projects } from '../data/projects'
 
 export default function Home({ data }) {
   return (
@@ -34,34 +35,33 @@ export default function Home({ data }) {
       <main>
         <section id='projects' className='min-h-screen flex justify-center items-center p-10'>
           <div className='w-full flex flex-col items-center'>
-            <h2 className='text-center mb-10'>Proyectos destacados</h2>
-            <div className='max-w-7xl flex justify-center gap-16 flex-wrap'>
-              <Card 
-                src="/images/expense-tracker.png" 
-                url="https://expenses-tracker-three-sooty.vercel.app/"
-                title="Expenses Tracker" 
-                description="Aplicacion de seguimiento de gastos, que integra firebase para guardar los datos y librerias para mostrar reportes."
-              />
-              <Card
-                src="/images/calculator.png"
-                url="https://trusting-heisenberg-37a0f2.netlify.app/" 
-                title="Calculadora"
-                description="Tipica app de calculadora con un diseño moderno."
-              />
-              <Card 
-                src="/images/pokedex.png" 
-                url="https://new-pokedex-cyan.vercel.app/"
-                title="Pokedex"
-                description="Aplicacion del clima construida con Svelte que nos trae la informacion relevante a traves de una api publica."
-              />
+            <h2 className='text-center'>Proyectos destacados</h2>
+            <p className='mb-16 max-w-2xl text-center'>Algunos de los proyectos que más me han gustado, en donde he podido utilizar mayor variedad de tecnologias  modernas</p>
+            <div className='max-w-7xl flex justify-center gap-16 flex-wrap mb-12'>
+              {projects
+                .filter((project) => project.isFavorite === true)
+                .map((project) => {
+                  return(
+                    <Card
+                      key={project.id}
+                      src={project.src} 
+                      url={project.url}
+                      title={project.title}
+                      description={project.description}
+                      tags={project.stack}
+                    />
+                )
+              })}
             </div>
+            <Link href='portfolio' className={styles.btnPrimary}>Ver todos</Link>
           </div>
         </section>
 
         <section className='p-10 min-h-screen flex justify-center items-center'>
           <div className='w-full flex flex-col items-center'>
-            <h2 className='text-center mb-10'>Ultimas publicaciones</h2>
-            <div className='max-w-7xl flex justify-center gap-16 flex-wrap'>
+            <h2 className='text-center'>Ultimas publicaciones</h2>
+            <p className='max-w-2xl text-center mb-16'>Quiero comenzar a entregar conocimiento a la comunidad, sobre todo a quienes estan recien empezando en el mundo de la programación</p>
+            <div className='max-w-7xl flex justify-center gap-16 flex-wrap mb-12'>
               <Card 
                 src="/images/expense-tracker.png" 
                 url="https://expenses-tracker-three-sooty.vercel.app/"
@@ -81,12 +81,13 @@ export default function Home({ data }) {
                 description="Aplicacion del clima construida con Svelte que nos trae la informacion relevante a traves de una api publica."
               />
             </div>
+            <Link href='portfolio' className={styles.btnPrimary}>Ver todos</Link>
           </div>
         </section>
 
         <section className='m-auto h-screen flex flex-col justify-center items-center max-w-2xl p-10'>
-          <h2 className='text-center mb-1'>Mis Skills</h2>
-          <p className='mb-12 text-center'>Los lenguajes y tecnologias donde más tengo experiencia</p>
+          <h2 className='text-center'>Mis Skills</h2>
+          <p className='mb-16 text-center'>Los lenguajes y tecnologias donde más tengo experiencia</p>
           <Stack />
         </section>
 
